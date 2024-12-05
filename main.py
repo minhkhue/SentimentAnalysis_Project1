42f3e89f93fe8b8c9fdbbbc94fadf3b63e76122c
minhkhue <jeanphan19851@yahoo.fr>
2024-12-05 15:52:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -105,11 +105,11 @@ if selected == 'Tải dữ liệu lên hệ thống':
     else:
         st.info('Dữ liệu đã được tải lên trước đó.')
         data = st.session_state['uploaded_data']  # Lấy dữ liệu từ session_state
-        st.markdown("<h1 style='text-align: center; color: white;'>5 dòng dữ liệu đầu tiên</h1>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>5 dòng dữ liệu đầu tiên</h1>", unsafe_allow_html=True)
         st.dataframe(data[['ma_khach_hang', 'ho_ten', 'ma_san_pham', 'ten_san_pham', 'mo_ta', 'diem_trung_binh', 'so_sao', 'noi_dung_binh_luan', 'ngay_binh_luan', 'gia_ban']].head(5))
         st.text("")
         st.text("")
-        st.markdown("<h1 style='text-align: center; color: white;'>5 dòng dữ liệu cuối cùng</h1>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>5 dòng dữ liệu cuối cùng</h1>", unsafe_allow_html=True)
         st.dataframe(data[['ma_khach_hang', 'ho_ten', 'ma_san_pham', 'ten_san_pham', 'mo_ta', 'diem_trung_binh', 'so_sao', 'noi_dung_binh_luan', 'ngay_binh_luan', 'gia_ban']].tail(5))
 # Giao diện phần 'Tổng quan về dataset'
 if selected == 'Tổng quan về dataset':
